734d9115c24be0838eb27cf919a0d886675c6dee
Leah Pronk <2109208@talnet.nl git config --global user.name Leah>
2019-11-22 15:38:56 +0100

    Uitwerking van de exercise

---
+++ upgraded_for.php
@@ -0,0 +1,37 @@
+<?php
+
+$studenten = array('Youssef','Kenza','George','Yari');
+
+$length = count($studenten);
+
+
+foreach ($studenten as $key => $value) {
+	print($value). 	PHP_EOL;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
